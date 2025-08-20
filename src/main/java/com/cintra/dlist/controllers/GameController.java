20f855ada2db95bc6b4e3ae21afade4393a9f53a
Eber Cintra <organico_51@hotmail.com>
2025-08-20 14:04:04 -0300

    get byId

--- src/main/java/com/cintra/dlist/controllers/GameController.java
+++ src/main/java/com/cintra/dlist/controllers/GameController.java
@@ -1,9 +1,11 @@
 package com.cintra.dlist.controllers;
 
+import com.cintra.dlist.dto.GameDto;
 import com.cintra.dlist.dto.GameMinDto;
 import com.cintra.dlist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +17,12 @@ public class GameController {
 
     @Autowired
     private GameService gameService;
+
+    @GetMapping(value = "/{id}")
+    public GameDto findById(@PathVariable Long id){
+        GameDto result = gameService.findById(id);
+        return result;
+    }
 
     @GetMapping
     public List<GameMinDto> findAll() {
